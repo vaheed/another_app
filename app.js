11c7abd2458f0668266735f1e6d1379af121cf22
vaheeD khoshnouD <khoshnud@gmail.com>
2024-07-24 11:59:40 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const os = require('os');
 const app = express();
 const port = 3000;
 
+// Function to get the local IP address
+function getLocalIpAddress() {
+  const interfaces = os.networkInterfaces();
+  for (const name of Object.keys(interfaces)) {
+    for (const iface of interfaces[name]) {
+      if (iface.family === 'IPv4' && !iface.internal) {
+        return iface.address;
+      }
+    }
+  }
+  return 'IP address not found';
+}
+
 app.get('/', (req, res) => {
-  res.send('Hello, vaheeD!');
+  const ipAddress = getLocalIpAddress();
+  res.send(`Hello, world! This app is running on IP address: ${ipAddress}`);
 });
 
 app.listen(port, () => {
